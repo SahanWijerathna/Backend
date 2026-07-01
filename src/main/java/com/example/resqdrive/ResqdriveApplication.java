aee31e673fd31054af25f1520da1c3acd8b81f34
Kasun Mihiran <kasunmihiran603@gmail.com>
2026-07-01 16:04:57 +0530

    ctreat spring boot

---
+++ src/main/java/com/example/resqdrive/ResqdriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.resqdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResqdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResqdriveApplication.class, args);
+	}
+
+}
